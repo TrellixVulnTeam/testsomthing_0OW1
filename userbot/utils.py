068000c592d250915bc7490200bf7ffedbac900f
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2022-06-25 11:40:40 +0000

    legend fixed

--- userbot/utils.py
+++ userbot/utils.py
@@ -20,12 +20,12 @@ from telethon import events, functions, types
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from userbot import CMD_LIST, LOAD_PLUG, SUDO_LIST, bot
+from userbot import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
 from userbot.Config import Config
 from userbot.helpers.exceptions import CancelProcess
 from userbot.helpers.tools import media_type
 from var import Var
-from userbot import LOGS
+
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
     from userbot.Config import Config
