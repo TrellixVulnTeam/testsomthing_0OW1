7dd09964f274188cec3a01421c72d12a5a3e1e4b
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-06-27 19:15:00 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -6,13 +6,17 @@ import os
 from telethon import TelegramClient
 from var import Var
 from userbot.Config import Config
-from userbot.utils import load_module
+from userbot.utils import load_module, start_assistant
 from userbot import LOAD_PLUG, LOGS, REBELversion
 from pathlib import Path
 import asyncio
 import telethon.utils
 
 os.system("pip install -U telethon")
+
+LOAD_ASSISTANT = os.environ.get("LOAD_ASSISTANT", True)
+REBEL_PIC = "https://telegra.ph/file/7661ea6e9c4040ca0ba04.jpg"
+
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
@@ -52,8 +56,36 @@ for name in files:
 
 import userbot._core
 
+if LOAD_ASSISTANT == True:
+    path = "userbot/assistant/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            try:
+                start_assistant(shortname.replace(".py", ""))
+            except Exception as er:
+                print(er)
+else:
+    print("Assitant is Not Loading As U Have Disabled")
+
+ 
 print(f"""Hello sir i am REBELBOT!! REBELBOT VERSION :- {REBELversion} YOUR REBELBOT IS READY! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @REBELBOT_SUPPORT .""")
 
+async def REBEL_is_on():
+    try:
+        if Config.REBELBOT_ID != 0:
+            await bot.send_file(
+                Config.REBELBOT_ID,
+                REBEL_PIC,
+                caption=f"༆ʟɛɢɛռɖaʀʏ ᴀғ ʀᴇʙᴇʟʙᴏᴛ༆\n\n**ᴠᴇʀsɪᴏɴ ➪ {REBELversion}**\n\n𝐓𝐲𝐩𝐞 `.ping` or `.alive` ᴛᴏ ᴄʜᴇᴄᴋ! \n\n ᴊᴏɪɴ [ʀᴇʙᴇʟʙᴏᴛ ᴄʜᴀᴛɪɴɢ](t.me/REBEL_BOT_CHATING) ᴛᴏ ǫᴜᴇʀʏ & ᴊᴏɪɴ [ʀᴇʙᴇʟʙᴏᴛ ᴜᴘᴅᴀᴛᴇ](t.me/REBELBOT_SUPPORT) ᴛᴏ ᴋɴᴏᴡ ʀᴇɢʀᴀᴅɪɴɢ ᴜᴘᴅᴀᴛᴇ ᴀɴᴅ ᴀʙᴏᴜᴛ ʀᴇʙᴇʟʙᴏᴛ",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+
+
+bot.loop.create_task(REBEL_is_on())
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
