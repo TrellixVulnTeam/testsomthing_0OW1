d2a157741e66d4ba70ec64f7d971f3ed735925fa
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-06-29 23:16:01 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,26 +1,24 @@
-import os
-from pathlib import Path
+from userbot import bot
 from sys import argv
-
-import telethon.utils
+import sys
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import os
 from telethon import TelegramClient
-
-from userbot import *
-from userbot import REBELversion, bot
-from userbot.Config import Config
-from userbot.utils import load_module, start_assistant
 from var import Var
+from userbot.Config import Config
+from userbot.utils import load_module
+from userbot import LOAD_PLUG, LOGS, REBELversion
+from pathlib import Path
+import asyncio
+import telethon.utils
 
 os.system("pip install -U telethon")
 
-LOAD_ASSISTANT = os.environ.get("LOAD_ASSISTANT", True)
-REBEL_PIC = "https://telegra.ph/file/7661ea6e9c4040ca0ba04.jpg"
-
-
 async def add_bot(bot_token):
     await bot.start(bot_token)
-    bot.me = await bot.get_me()
+    bot.me = await bot.get_me() 
     bot.uid = telethon.utils.get_peer_id(bot.me)
+
 
 
 if len(argv) not in (1, 3, 4):
@@ -31,7 +29,9 @@ else:
         print("Initiating Inline Bot")
         # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished with no errors")
         print("🔰ՏTᗩᖇT ᖇᗴᗷᗴᒪᗷOT🔰")
@@ -42,8 +42,7 @@ else:
 
 
 import glob
-
-path = "userbot/plugins/*.py"
+path = 'userbot/plugins/*.py'
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
@@ -51,42 +50,11 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
+import userbot._core
 
-if LOAD_ASSISTANT == True:
-    path = "userbot/assistant/*.py"
-    files = glob.glob(path)
-    for name in files:
-        with open(name) as f:
-            path1 = Path(f.name)
-            shortname = path1.stem
-            try:
-                start_assistant(shortname.replace(".py", ""))
-            except Exception as er:
-                print(er)
-else:
-    print("Assitant is Not Loading As U Have Disabled")
+print(f"""Hello sir i am REBELBOT!! REBELBOT VERSION :- {REBELversion} YOUR REBELBOT IS READY! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @REBELBOT_SUPPORT .""")
 
-
-print(
-    f"""Hello sir i am REBELBOT!! REBELBOT VERSION :- {REBELversion} YOUR REBELBOT IS READY! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @REBELBOT_SUPPORT ."""
-)
-
-
-async def REBEL_is_on():
-    try:
-        if Config.REBELBOT_ID != 0:
-            await bot.send_file(
-                Config.REBELBOT_ID,
-                REBEL_PIC,
-                caption=f"༆ʟɛɢɛռɖaʀʏ ᴀғ ʀᴇʙᴇʟʙᴏᴛ༆\n\n**ᴠᴇʀsɪᴏɴ ➪ {REBELversion}**\n\n𝐓𝐲𝐩𝐞 `.ping` or `.alive` ᴛᴏ ᴄʜᴇᴄᴋ! \n\n ᴊᴏɪɴ [ʀᴇʙᴇʟʙᴏᴛ ᴄʜᴀᴛɪɴɢ](t.me/REBEL_BOT_CHATING) ᴛᴏ ǫᴜᴇʀʏ & ᴊᴏɪɴ [ʀᴇʙᴇʟʙᴏᴛ ᴜᴘᴅᴀᴛᴇ](t.me/REBELBOT_SUPPORT) ᴛᴏ ᴋɴᴏᴡ ʀᴇɢʀᴀᴅɪɴɢ ᴜᴘᴅᴀᴛᴇ ᴀɴᴅ ᴀʙᴏᴜᴛ ʀᴇʙᴇʟʙᴏᴛ",
-            )
-    except Exception as e:
-        LOGS.info(str(e))
-
-
-bot.loop.create_task(REBEL_is_on())
-if len(argv) in {1, 3, 4}:
-    bot.run_until_disconnected()
-
-else:
+if len(argv) not in (1, 3, 4):
     bot.disconnect()
+else:
+    bot.run_until_disconnected()
