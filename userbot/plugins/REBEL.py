a7a70d40b4c6eeaf6153a51eb07fe2016c6e2aed
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-06-29 18:58:03 +0530

    Update REBEL.py

--- userbot/plugins/REBEL.py
+++ userbot/plugins/REBEL.py
@@ -3,7 +3,7 @@ import time
 from REBELBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 from userbot import ALIVE_NAME, StartTime
-
+from userbot.Config import Config
 
 async def reply_id(event):
     reply_to_id = None
