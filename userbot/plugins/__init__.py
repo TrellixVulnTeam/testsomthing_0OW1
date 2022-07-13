1101c3e3e763cd156b18598ee6659db4eaf09d71
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-07-14 00:16:11 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,5 +1,5 @@
 from userbot import *
 from userbot.cmdhelp import CmdHelp
-from userbot.Config import Config
+from userbot.smex.DARK_Config import Config
 from userbot.helpers.functions import *
 from userbot.utils import *
