3df8eab691d095e1e6d8369934ac6f73295954a8
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-07-13 23:56:13 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,7 +5,7 @@ import time
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-from userbot.Config.DARK_Config import Config
+from userbot.Config import Config
 from userbot.helpers import functions as simpdef
 from var import Var
 
