5b1cd5efc336bb1f6f7abf6de68a8c141a2ca59e
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-07-14 19:47:57 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -17,7 +17,7 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    event = await edit_or_reply(event, "__**(❛ ᑭσɳց ❜!__**")
+    event = await edit_or_reply(event, "__**❛ ᑭσɳց ❜!__**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     if DARK_IMG:
