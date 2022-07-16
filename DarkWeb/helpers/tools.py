d9693093b25019cbbb5e19ebf528e10b6d3710a6
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-07-16 20:07:57 +1000

    Add files via upload

---
+++ DarkWeb/helpers/tools.py
@@ -0,0 +1,98 @@
+import functools
+
+from DarkWeb import bot
+
+
+def media_type(message):
+    if message:
+        if message.photo:
+            return "Photo"
+        if message.audio:
+            return "Audio"
+        if message.voice:
+            return "Voice"
+        if message.video_note:
+            return "Round Video"
+        if message.gif:
+            return "Gif"
+        if message.sticker:
+            return "Sticker"
+        if message.video:
+            return "Video"
+        if message.document:
+            return "Document"
+    return None
+
+
+def forwards():
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(event):
+            if event.fwd_from:
+                await func(event)
+
+        return wrapper
+
+    return decorator
+
+
+def iadmin():
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(event):
+            myid = await bot.get_me()
+            myperm = await bot.get_permissions(event.chat_id, myid)
+            if myperm.is_admin:
+                await func(event)
+            if myperm.is_creator:
+                await func(event)
+            else:
+                await event.edit("I'm not admin. Chutíya sala.")
+
+        return wrapper
+
+    return decorator
+
+
+def if_bot():
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(event):
+            reply_msg = await event.get_reply_message()
+            reply_msg.sender
+            if not reply_msg.sender.bot:
+                await func(event)
+            else:
+                await event.edit("That's a Bot I Guess. Please reply to actual users..")
+
+        return wrapper
+
+    return decorator
+
+
+def pm_limit():
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(event):
+            if event.is_group:
+                await func(event)
+            else:
+                await event.edit(
+                    "This Command Only Works In Groups. Try again in a group.."
+                )
+
+        return wrapper
+
+    return decorator
+
+
+def no_grp():
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(event):
+            if not event.is_group:
+                await func(event)
+
+        return wrapper
+
+    return decorator
